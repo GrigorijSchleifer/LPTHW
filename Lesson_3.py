dbd7c5c2ba1aaabf90c7362c046d958f61be3ad9
Grigorij Schleifer <chaosambulance@googlemail.com>
2022-08-09 20:08:08 +0200

    lesson 4

--- Lesson_3.py
+++ Lesson_3.py
@@ -8,10 +8,10 @@ print("Now I will count the eggs:")
 print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
 
 print("Is it true that: 3 + 2 < 5 - 7?")
-hjjjj
+
 print(3 + 2 < 5 - 7)
 
 print("What is 3 + 2?", 3 + 2)
-llprint("What is 3 - 7?", 5 - 7)
+print("What is 3 - 7?", 5 - 7)
 
 print("Oh, that's why it's False.")
