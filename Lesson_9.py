12080345a9f5b3dd50fe4ea0f4afdc256d34acad
Grigorij Schleifer <chaosambulance@googlemail.com>
2022-08-17 23:25:59 +0200

    Lesson 8 and 9

---
+++ Lesson_9.py
@@ -0,0 +1,13 @@
+days = "Mon Tue Wed Fri Sat Sun"
+months = "Jan\nFeb\nMar\nApr\nJun\nJul\nAug"
+
+print("Here are the days", days)
+print("Here are the month", months)
+
+print("""
+"Ther's something going on here.
+With the three double-quotes.
+We'll be able to type as much as we like
+Even 4 lines if we want, or 5, or 6.
+"""
+)
